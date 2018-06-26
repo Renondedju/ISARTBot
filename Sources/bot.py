3fa5800140dec5ba5ab122f615a7710ed495de52
Basile Combet <b.combet@student.isartdigital.com>
2018-06-26 14:04:57 +0200

    Fixed some bot errors prints

--- Sources/bot.py
+++ Sources/bot.py
@@ -57,12 +57,16 @@ class Bot(discord.ext.commands.Bot):
 
         #Setup
         for name, enabled in self.__commands.items():
-            if (name != "" and enabled):
-                self.load_extension  ('commands.' + name)
-                self.__logs.print    ('Loaded the command ' + name)
-            else:
-                self.__logs.print    ('The command ' + name + ' is currently disabled')
-        
+            if (name != ""):
+                name = name.strip('_')
+                try:
+                    self.load_extension  ('commands.' + name)
+                    self.__logs.print    ('Loaded the command {0}, enabled = {1}'
+                        .format(name, str(enabled.get('enabled'))))
+                except:
+                    self.__logs.print('Failed to load extension named command.{0}'
+                    .format(name))
+
         self.add_check(self.globally_block_dms)
         self.add_check(self.log_command)
         self.add_check(self.check_enable)
@@ -79,6 +83,7 @@ class Bot(discord.ext.commands.Bot):
             to discord and ready to operate
         """
 
+        self.__logs.print('------------')
         self.__logs.print('Logged in as')
         self.__logs.print('Username : {0}#{1}'.format(self.user.name, self.user.discriminator))
         self.__logs.print('User ID  : {0}'    .format(self.user.id))
@@ -111,6 +116,8 @@ class Bot(discord.ext.commands.Bot):
         # All other Errors not returned come here... And we can just print the default TraceBack.
         self.__logs.print('Ignoring exception in command {}:'.format(ctx.command))
         for err in traceback.format_exception(type(error), error, error.__traceback__):
+            if (err[len(err) - 1] == '\n'):
+                err = err[:-1]
             self.__logs.print(err)
 
         return
