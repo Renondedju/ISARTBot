ff2950e0ddac947d783b8b69865753881da9969f
Basile Combet <b.combet@student.isartdigital.com>
2018-06-27 16:54:47 +0200

    Added the game create command

--- Sources/commands/game.py
+++ Sources/commands/game.py
@@ -42,29 +42,79 @@ class Game_commands():
         #Private
         self.__bot = bot
 
-    @commands.group(pass_context=True)
+    @commands.group(pass_context=True, invoke_without_command=True)
     async def game(self, ctx):
-        """
-            Creates a command group
-        """
+        """ Creates a command group """
 
         if ctx.invoked_subcommand is None:
-            await ctx.send("Game isn't a command, if you need some help please"
+            await ctx.send("Game requiers a subcommand, if you need some help please"
                            " type ``{}help game``"
                            .format(self.__bot.settings.get("bot", "prefix")))
 
-    @game.command(name='create')
+    @game.command(name='create', hidden=True)
     @commands.check(is_admin)
-    async def _create(self, ctx, game_name: str):
-        """
-            Creates a game if this game does not exists yet
-        """
+    async def _create(self, ctx, *, game_name: str):
+        """ Creates a game if this game does not exists yet """
 
-        await ctx.send(game_name)
+        game_name = game_name.lower().strip()
+
+        #Checking if the game already exists
+        if game_name in self.__bot.settings.get("games_roles", command="game"):
+            await ctx.send("The game named {} already exists !".format(game_name))
+            return
+
+        #Retriving the game category
+        game_category = None
+        for cat in ctx.guild.categories:
+            if str(cat.id) == str(ctx.bot.settings.get("category_id", command="game")):
+                game_category = cat
+                break
+
+        #If the game category isn't found, raising an error
+        if (game_category is None):
+            raise commands.CommandError(message="No valid category id found. Aborting")
+
+        #Otherwise creating it
+        color = ctx.bot.settings.get("role_color", command="game")
+        role = await ctx.guild.create_role(
+            name        = game_name,
+            colour      = discord.Colour(int(color, 16)),
+            mentionable = True)
+
+        ctx.bot.settings.write({"id" : role.id}, role.name, "games_roles", command="game")
+
+        override = {role                   : discord.PermissionOverwrite(read_messages=True),
+                    ctx.guild.default_role : discord.PermissionOverwrite(read_messages=False)}
+
+        #Creating the text and vocal channels
+        text  = await ctx.guild.create_text_channel (
+            name       = game_name,
+            category   = game_category,
+            overwrites = override)
+
+        vocal = await ctx.guild.create_voice_channel(
+            name       = game_name,
+            category   = game_category,
+            overwrites = override)
+
+        if (text is None or vocal is None):
+            raise commands.CommandError(message="Failed to create the game channels")
+
+        await ctx.send("Added the game {} to the list of avaliable games".format(role.mention))
+
         return
 
+    @game.command(name='delete', hidden=True)
+    @commands.check(is_admin)
+    async def _delete(self, ctx, *, game_name: str):
+
+        game_name = game_name.lower().strip()
+
+        await ctx.send("TODO :)")
+
+    @_delete.error
     @_create.error
-    async def _create_error(self, ctx, error):
+    async def _error(self, ctx, error):
         """
             Catches errors of the create subcommand
         """
@@ -76,8 +126,12 @@ class Game_commands():
             await ctx.send("You missed an argument. Type __``{}help"
                             "game create``__ for some help"
                             .format(self.__bot.settings.get("bot", "prefix")))
+
+        elif isinstance(error, commands.MissingPermissions):
+            await ctx.send("I need some more permissions to do that sorry !")
+
         else:
-            raise error
+            await ctx.bot.on_error(ctx, error)
 
         return
 
