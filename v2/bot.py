132e0cee60bc03fbd48b4ace6cca610ac094a322
Renonedeju <lefantomedelaredstone@gmail.com>
2019-07-30 17:22:12 +0200

    [Added] Module loading and command logging

--- v2/bot.py
+++ v2/bot.py
@@ -51,21 +51,59 @@ class Bot(discord.ext.commands.Bot):
         # Loading settings
         self.logger.info('Settings file located at {}'.format(self.config_file))
         self.settings = configparser.ConfigParser(converters={'list': lambda x: [i.strip() for i in x.split(',')]})
-        self.settings.read(abspath('./settings.ini'))
+        self.settings.read(self.config_file)
 
         self.modules        = self.settings['modules']
         self.command_prefix = self.settings.get('common', 'prefix')
 
+        self.add_check(self.log_command)
+
+        self.loop.create_task(self.load_modules())
+
         self.run(self.settings.get('common', 'token'))
 
+    async def load_modules(self):
+        """ Loads all the cogs of the bot defined into the settings.ini file """
 
-#        #setting up logger
-#       logging.basicConfig(
-#           level=logging.INFO,
-#           format="%(asctime)s - [%(levelname)s] %(message)s",
-#           datefmt="%Y-%m-%d %H:%M:%S",
-#           handlers=[
-#               logging.StreamHandler(sys.stdout),
-#               logging.FileHandler("logs.log", mode='a', encoding=None, delay=False)
-#           ]
-#       )
+        try:
+            await self.wait_for('ready', timeout=30)
+            self.logger.info("Loading modules...")
+        except asyncio.futures.TimeoutError:
+            self.logger.warning("Wait for on_ready event timed out, loading the cogs anyway.")
+
+        for (module, enabled) in self.settings.items('modules'):
+            try:
+                self.load_extension('v2.modules.' + module)
+                self.logger.info   ('Loaded the module {} : enabled = {}'.format(module, self.modules.getboolean(module)))
+
+            except Exception as e:
+                await self.on_error(None, e)
+                self.logger.error('Failed to load extension named modules.{0}'.format(name))
+
+        return
+
+    # --- Events ---
+
+    async def on_ready(self):
+        """
+            Executed when the bot is connected
+            to discord and ready to operate
+        """
+
+        self.logger.info('------------')
+        self.logger.info('Logged in as')
+        self.logger.info('Username : {0}#{1}'.format(self.user.name, self.user.discriminator))
+        self.logger.info('User ID  : {0}'    .format(self.user.id))
+        self.logger.info('------------')
+
+    # --- Checks ---
+
+    async def log_command(self, ctx):
+        """ Logs every command """
+
+        author  = '{0}#{1}'     .format(ctx.author.name, ctx.author.discriminator)
+        channel = '{2.name}/{1.name}/{0}'.format(ctx.channel.name, ctx.channel.category, ctx.guild)
+
+        self.logger.info('{0} -> {1} : {2}'.format(author, channel, ctx.message.content))
+
+        return True
