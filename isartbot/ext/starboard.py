2352b5a320bd97eb95dea9c2548fda3297d9ac20
Renonedeju <lefantomedelaredstone@gmail.com>
2019-08-10 18:13:35 +0200

    [WIP] Starboard

--- isartbot/ext/starboard.py
+++ isartbot/ext/starboard.py
@@ -22,6 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import emoji
 import discord
 
 from discord.ext import commands
@@ -32,11 +33,17 @@ from isartbot.models.server_preferences import ServerPreferences
 class StarboardExt(commands.Cog):
     """ Starboard related commands and tasks """
 
-    __slots__ = ("bot")
+    __slots__ = ("bot", "stars")
 
     def __init__(self, bot, *args, **kwargs):
 
         self.bot = bot
+
+        self.minimum_stars = int(self.bot.settings.get("starboard", "minimum_stars"))
+
+        # Sorting the stars (and conveting the keys to integers)
+        self.stars = {int(k):v for k,v in self.bot.settings.items("starboard_icons")}
+        self.stars = dict(sorted(self.stars.items()))
 
     # Commands
 
@@ -97,15 +104,131 @@ class StarboardExt(commands.Cog):
 
         await ctx.send(embed=embed)
 
+    # Methods
+
+    def is_starboard_message(self, message: discord.Message) -> bool:
+        """ Checks if the message is a starboard message """
+
+        if message is None:
+            return False
+
+        # If the message is in a starboard channel
+        # And if the author is the bot
+        # and if the message has embeds
+        if (message.author.id  == self.bot.user.id and
+            message.channel.id == await self.get_server_starboard_channel_id(message.guild) and
+            len(message.embeds) > 0):
+
+            # If the author url of the first embed of the message
+            # starts with 'https://discordapp.com/channels/', then it's
+            # a starboard message
+            return str(message.embeds[0].author.url).startswith('https://discordapp.com/channels/')
+
+        return False
+
+    def get_emoji_message(self, message: discord.Message, star_count : int):
+        """ Returns the starboarded version of a message """
+
+        emoji   = self.get_star_emoji(star_count)
+        content = f'{emoji} **{star_count}** {message.channel.mention}'
+
+        embed = discord.Embed(description=message.content)
+
+        # Adding image or attachments
+        if message.embeds:
+            data = message.embeds[0]
+            if data.type == 'image':
+                embed.set_image(url=data.url)
+
+        if message.attachments:
+            file = message.attachments[0]
+            if file.url.lower().endswith(('png', 'jpeg', 'jpg', 'gif', 'webp')):
+                embed.set_image(url=file.url)
+            else:
+                embed.add_field(name='Attachment', value=f'[{file.filename}]({file.url})', inline=False)
+
+        embed.timestamp = message.created_at
+        embed.colour    = discord.Color.gold()
+        embed.set_author(name     = message.author.display_name, 
+                         url      = message.jump_url,
+                         icon_url = message.author.avatar_url_as(format='png'))
+
+        return content, embed
+
+    def get_star_emoji(self, star_count : int) -> str:
+        """ Returns the emoji to use of the count of stars """
+
+        # Default emoji is :star:
+        emoji = ":star:"
+
+        # Since the self.stars dict has been sorted, we can check
+        # for each star icon if the star_count variable is high enough to use it.
+        for (reaction_count, emoji_name) in self.stars.items():
+            if star_count >= reaction_count:
+                emoji = emoji_name
+            else:
+                return emoji
+
+        return emoji
+
+    async def get_server_starboard_channel_id(self, server: discord.Guild) -> int:
+        """ Returns the setuped starboard channel id for a given server """
+
+        result = await self.bot.database.connection.execute(
+                ServerPreferences.table.select(ServerPreferences.table.c.discord_id == server.id)
+            )
+        servers = await result.fetchall()
+
+        # Something is wrong, either the server is not registered in the database, 
+        # either there is more than one server with this id, which shouldn't be possible
+        if (len(servers) != 1):
+            self.bot.logger.warning("Starboard configuration ambiguity ! Please check the database integrity.")
+            return 0
+
+        server = servers[0]
+
+        return server[ServerPreferences.table.c.starboard_id]
+
+    async def get_server_starboard_channel(self, server: discord.Guild) -> discord.TextChannel:
+
+        channel_id = await self.get_server_starboard_channel_id(server)
+
+        if (channel_id != 0):
+            return server.get_channel(channel_id)
+
+        return None
+
+    async def is_reaction_eligible(self, reaction) -> bool:
+        """ Checks if a reaction is eligible for the starboard """
+
+        # Retreiving the emoji code ex.: ":star:"
+        reaction_emoji = reaction.emoji
+        if isinstance(reaction_emoji, str):
+            reaction_emoji = emoji.demojize(reaction_emoji)
+        else:
+            reaction_emoji = reaction_emoji.name
+
+        if (reaction_emoji == self.bot.settings.get('starboard', 'control_emoji')):
+            if (await self.get_server_starboard_channel_id(reaction.message.guild) != 0):
+                return True
+        
+        return False
+
     # Events
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        self.bot.logger.info(f"Added reaction: {reaction.emoji}, {user.name}")
+        self.bot.logger.info(f"Added reaction: {reaction.emoji}, {user.name}, {await self.is_reaction_eligible(reaction)}")
+
+        if (await self.is_reaction_eligible(reaction)):
+            content, embed = self.get_emoji_message(reaction.message, 1)
+            channel = await self.get_server_starboard_channel(reaction.message.guild)
+
+            await channel.send(content = content, embed = embed)
 
     @commands.Cog.listener()
     async def on_reaction_remove(self, reaction, user):
-        self.bot.logger.info(f"Removed reaction: {reaction.emoji}, {user.name}")
+        self.bot.logger.info(f"Removed reaction: {reaction.emoji}, {user.name} {await self.is_reaction_eligible(reaction)}")
 
     @commands.Cog.listener()
     async def on_reaction_clear(self, message, reactions):
