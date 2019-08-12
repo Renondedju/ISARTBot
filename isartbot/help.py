960b69d525fd375804d4619e83f33a63f87fbf28
Renonedeju <lefantomedelaredstone@gmail.com>
2019-08-12 18:11:50 +0200

    [Done] Help command

--- isartbot/help.py
+++ isartbot/help.py
@@ -28,28 +28,42 @@ from discord.ext import commands
 
 class HelpCommand(commands.MinimalHelpCommand):
 
-    __slots__ = ("opening_note")
+    __slots__ = ("opening_note", "title")
 
-    def __init__(self, **options):
+    def __init__(self):
 
         self.opening_note = ""
+        self.title        = "Help"
 
-        # Forcing the checks verification
-        options['verify_checks'] = True
-        
-        super().__init__(**options)
+        super().__init__(verify_checks=True, paginator=commands.Paginator(prefix=None, suffix=None, max_size=1900))
+
+    async def send_pages(self):
+        """A helper utility to send the page output from :attr:`paginator` to the destination."""
+
+        destination = self.get_destination()
+
+        for page in self.paginator.pages:
+            embed = discord.Embed(
+                title       = self.title,
+                description = page
+            )
+
+            await destination.send(embed=embed)
 
     async def prepare_help_command(self, ctx, command):
         """ Prepares the help command """
 
-        self.opening_note     = await self.context.bot.get_translation(self.context, "help_opening_note")
-        self.commands_heading = await self.context.bot.get_translation(self.context, "commands_heading")
+        translations = await self.context.bot.get_translations(self.context, ("help_opening_note", "commands_heading", "help_title"))
+
+        self.opening_note     = translations.pop("help_opening_note", "")
+        self.commands_heading = translations.pop("commands_heading" , "")
+        self.title            = translations.pop("help_title"       , "Help")
 
         await super().prepare_help_command(ctx, command)
 
     def get_opening_note(self):
         """ Returns help command's opening note. This is mainly useful to override for i18n purposes. """
-        
+
         return self.opening_note.format(self.clean_prefix, self.invoked_with)
 
     async def send_bot_help(self, mapping):
@@ -60,7 +74,7 @@ class HelpCommand(commands.MinimalHelpCommand):
             self.paginator.add_line(note, empty=True)
 
         filtered = await self.filter_commands(self.context.bot.commands)
-        
+
         self.paginator.add_line('**%s**' % self.commands_heading)
         for command in filtered:
             self.paginator.add_line(f"{self.clean_prefix}{command.name}")
@@ -73,7 +87,9 @@ class HelpCommand(commands.MinimalHelpCommand):
         await self.send_pages()
 
     async def send_group_help(self, group):
-        self.add_command_formatting(group)
+        """ Sends the command group help """
+
+        await self.add_command_formatting(group)
 
         filtered = await self.filter_commands(group.commands, sort=self.sort_commands)
         if filtered:
@@ -83,7 +99,7 @@ class HelpCommand(commands.MinimalHelpCommand):
 
             self.paginator.add_line('**%s**' % self.commands_heading)
             for command in filtered:
-                self.add_subcommand_formatting(command)
+                await self.add_subcommand_formatting(command)
 
             note = self.get_ending_note()
             if note:
@@ -93,6 +109,46 @@ class HelpCommand(commands.MinimalHelpCommand):
         await self.send_pages()
 
     async def send_command_help(self, command):
-        self.add_command_formatting(command)
+        """ Sends the help command """
+
+        await self.add_command_formatting(command)
         self.paginator.close_page()
+
         await self.send_pages()
+
+    async def add_command_formatting(self, command):
+        """ A utility function to format commands and groups. """
+
+        bot = self.context.bot
+
+        signature = self.get_command_signature(command)
+        if command.aliases:
+            self.paginator.add_line(signature)
+            self.add_aliases_formatting(command.aliases)
+        else:
+            self.paginator.add_line(signature, empty=True)
+
+        if command.description:
+            self.paginator.add_line(await bot.get_translation(self.context, command.description), empty=True)
+
+    async def add_subcommand_formatting(self, command):
+        """Adds formatting information on a subcommand."""
+
+        bot  = self.context.bot
+        fmt  = '{0}{1} - {2}' if command.short_doc else '{0}{1}'
+        line = fmt.format(self.clean_prefix, command.qualified_name, await bot.get_translation(self.context, command.short_doc))
+
+        self.paginator.add_line(line)
+
+    async def command_not_found(self, string):
+        """ A method called when a command is not found in the help command. This is useful to override for i18n. """
+
+        return (await self.context.bot.get_translation(self.context, "command_not_found")).format(string)
+
+    async def subcommand_not_found(self, command, string):
+        """ A method called when a command did not have a subcommand requested in the help command. This is useful to override for i18n. """
+
+        if isinstance(command, commands.Group) and len(command.all_commands) > 0:
+            return (await self.context.bot.get_translation(self.context, "no_subcommand_named")).format(command, string)
+
+        return (await self.context.bot.get_translation(self.context, "no_subcommand")).format(command)
