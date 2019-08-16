28f8945cdcba0e9e2a34cec29f5425eb7782e329
Thomas <th.colin@student.isartdigital.com>
2019-08-16 22:21:29 +0200

    [Improve] Moderation messages

--- isartbot/ext/moderation.py
+++ isartbot/ext/moderation.py
@@ -24,9 +24,12 @@
 
 import discord
 import asyncio
+import re
+import io
 
 from discord.ext     import commands
 from isartbot.checks import is_moderator
+from discord.abc     import Messageable
 
 class ModerationExt(commands.Cog):
     """Helps with moderation"""
@@ -35,6 +38,22 @@ class ModerationExt(commands.Cog):
 
         #Private
         self.bot = bot
+
+    async def error_embed(self, ctx, description: str, *args):
+        """Create an error embed"""
+
+        return discord.Embed(
+            title       = await ctx.bot.get_translation(ctx, "failure_title"),
+            description = (await ctx.bot.get_translation(ctx, description)).format(*args),
+            color       = discord.Color.red())
+
+    async def success_embed(self, ctx, description: str, *args):
+        """Create a success embed"""
+
+        return discord.Embed(
+            title       = await ctx.bot.get_translation(ctx, 'success_title'),
+            description = (await ctx.bot.get_translation(ctx, description)).format(*args),
+            color       = discord.Color.green())
 
     @commands.group(pass_context=True)
     @commands.check(is_moderator)
@@ -45,66 +64,159 @@ class ModerationExt(commands.Cog):
     #Prune command
     @mod.command(help="mod_prune_help", description="mod_prune_description")
     @commands.bot_has_permissions(manage_messages=True, read_message_history=True)
+    @commands.has_permissions    (manage_messages=True, read_message_history=True)
     async def prune(self, ctx, number: int, member: discord.Member = None):
         """Deletes a certain amount of the latest messages in this text channel"""
 
         embed = discord.Embed()
-        embed.title = "Sucess"
-        embed.colour = discord.Color.green()
-
         messages = []
         if (member is None):
-            embed.description = f"Last {number} messages has been removed"
+            embed = await self.success_embed(ctx, 'success_prune_message', number)
             messages = await ctx.channel.history(limit=number + 1).flatten()
         else:
             async for message in ctx.channel.history(limit=number + 1):
                 if (message.author == member):
                      messages.append(message)
             messages.append(ctx.message)
-            embed.description = f"Last {number} messages from {member.mention} has been removed"
+            embed = await self.success_embed(ctx, 'success_prune_member', number, member.mention)
 
         await ctx.channel.delete_messages(set(messages))
-
-        await ctx.send(embed=embed)
+            
+        await ctx.send(embed=embed, delete_after=5.0)
 
     #Kick command
     @mod.command(help="mod_kick_help", description="mod_kick_description")
     @commands.bot_has_permissions(kick_members=True)
+    @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member: discord.Member, *, reason="Because"):
         """Kicks a member"""
 
-        embed = discord.Embed()
+        #await member.kick(reason=reason)
 
-        await member.kick(reason=reason)
+        file = discord.File("isartbot/media/kick.gif", filename="kick.gif")
+        embed = await self.success_embed(ctx, 'success_kick', member.mention, ctx.author.mention, reason)
+        embed.set_image(url="attachment://kick.gif")
 
-        string = f"{member.mention} was kicked by {ctx.author.mention} for reason: {reason}"
+        self.bot.logger.warning((await ctx.bot.get_translation(ctx, 'success_kick')).format(member.name, ctx.author.name, reason))
 
-        embed.title = "Success"
-        embed.description = string
-        embed.colour = discord.Color.green()
-
-        self.bot.logger.info(string)
-
-        await ctx.send(embed=embed)
+        await ctx.send(file=file, embed=embed)
 
     #Ban command
-    @mod.command(aliases=["banhammer", "derive_autoritaire", "restriction_de_la_liberte_d_expression"], help="mod_ban_help", description="mod_ban_description")
+    @mod.command(aliases=["banhammer", "derive_autoritaire"], help="mod_ban_help", description="mod_ban_description")
     @commands.bot_has_permissions(ban_members=True)
+    @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member: discord.Member, *, reason="Nothing special"):
         """Bans a member"""
+
         await member.ban(reason=reason, delete_message_days=0)
 
-        embed = discord.Embed()
+        fileHammer = discord.File("isartbot/media/banhammer.gif", filename="banhammer.gif")
+        fileSalty = discord.File("isartbot/media/saltyban.gif", filename="saltyban.gif")
+        
+        embed = await self.success_embed(ctx, 'success_ban', member.mention, ctx.author.mention, reason)
+        embed.set_image(url="attachment://banhammer.gif")
 
-        string = f"{member} was banned by {ctx.author} for reason: {reason}"
+        if (ctx.invoked_with == 'derive_autoritaire'):
+            embed.set_image(url="attachment://saltyban.gif")
+            await ctx.send(file=fileSalty, embed=embed)
+        else:
+            embed.set_image(url="attachment://banhammer.gif")
+            await ctx.send(file=fileHammer, embed=embed)
 
-        embed.title = "Success"
-        embed.description = string
-        embed.colour = discord.Color.green()
+        self.bot.logger.warning((await ctx.bot.get_translation(ctx, 'success_ban')).format(member.name, ctx.author.name, reason))
 
-        self.bot.logger.info(string)
-
-        await ctx.send(embed=embed)
+#    @commands.command(help="mod_as_help", description="mod_as_description", name='as')
+#    @commands.check(is_moderator)
+#    async def _as(self, ctx, member : discord.Member, *, command_str : str):
+#
+#        prefix = self.bot.clean_prefix
+#        regex  = r"{0}(\w*)\s.*".format(prefix)
+#        matches = re.search(regex, command_str)
+#
+#        embedFail = discord.Embed()
+#        embedFail.title = "Failed"
+#        embedFail.colour = discord.Color.red()
+#
+#        if not matches:
+#            embedFail.description = 'There is no command to invoke.'
+#            await ctx.send(embed=embed)
+#            return
+#
+#        command = self.bot.get_command(matches.group(1))
+#        if (not await command.can_run(ctx)):
+#            embedFail.description = "The checks for this command failed, "
+#                                    "maybe you don't have the required rights ?"
+#            await ctx.send(embed=embed)
+#            return
+#
+#        for word in re.compile('\w+').findall(command_str.replace(prefix + matches.group(1), '')):
+#
+#            # if type of 'command' is a discord command, then everything is ready to be executed
+#            if type(command) == discord.ext.commands.core.Command:
+#                break
+#
+#            command = command.get_command(word)
+#            if command is None:
+#                break
+#            if (not await command.can_run(ctx)):
+#                await self.bot.send_fail(ctx,
+#                                "The checks for this subcommand failed, "
+#                                "maybe you don't have the required rights?")
+#                return
+#
+#        msg: discord.Message = await ctx.send(command_str)
+#        msg.author           = member
+#        await self.bot.process_commands(msg)
+#
+#        return
+#
+#    @commands.command(pass_context=True, hidden=True, name='for',
+#        usage="!for <Users and/or Roles> <'add' or 'remove'> <Roles>")
+#    @commands.check(is_admin)
+#    async def _for(self, ctx, *args : Union[discord.Member, discord.Role, str]):
+#        """Removes or adds roles to members of the guild"""
+#        selectors, action, roles = [list(items) for _, items in itertools.groupby(args, key=lambda x: isinstance(x, str))]
+#
+#        if len(action) != 1:
+#            await self.bot.send_fail(ctx, "Action should be 'add' or 'remove'!", 'Error')
+#            return
+#
+#        action = action[0].lower()
+#
+#        if action not in ['add', 'remove']:
+#            await self.bot.send_fail(ctx, "Action should be 'add' or 'remove'!", 'Error')
+#            return
+#
+#        selected_members = set()
+#
+#        for selection in selectors:
+#            if isinstance(selection, discord.Member):
+#                selected_members.add(selection)
+#                continue
+#
+#            for member in selection.members:
+#                selected_members.add(member)
+#
+#        async def add():
+#            for member in selected_members:
+#                for role in roles:
+#                    await member.add_roles(role)
+#
+#        async def remove():
+#            for member in selected_members:
+#                for role in roles:
+#                    await member.remove_roles(role)
+#
+#        strategies = {'add' : add, 'remove' : remove}
+#
+#        await strategies[action]()
+#
+#        await self.bot.send_success(ctx,
+#            'Used the \'{}\' strategy with the roles {} on {}'.format(
+#                action,
+#                ' '.join([role.mention for role in roles]),
+#                ' '.join([selector.mention for selector in selectors])),
+#            'For command')
 
 def setup(bot):
     bot.add_cog(ModerationExt(bot))
