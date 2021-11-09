f7b35bb75906247635429b75c9819cfac200d6ac
Renondedju <21277516+Renondedju@users.noreply.github.com>
2021-11-09 13:41:22 +0100

    Removed the diffusion extension (#10)

--- isartbot/converters/__init__.py
+++ isartbot/converters/__init__.py
@@ -22,11 +22,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-__slots__ = ("upper_clean", "ClassConverter", "GameConverter", "MemberConverter", "DiffusionConverter")
+__slots__ = ("upper_clean", "ClassConverter", "GameConverter", "MemberConverter")
 
 from .upper_clean           import upper_clean
 from .class_converter       import ClassConverter
 from .game_converter        import GameConverter
 from .member_converter      import MemberConverter
-from .diffusion_converter   import DiffusionConverter
 from .better_role_converter import BetterRoleConverter
--- isartbot/database/models/server.py
+++ isartbot/database/models/server.py
@@ -40,6 +40,5 @@ class Server(TableBase):
     starboard_minimum    = Column('starboard_minimum', Integer, default     = 3)
     verified_role_id     = Column('verified_role_id' , Integer, default     = 0)
     
-    games                 = relationship('Game'                 , cascade='all,delete,delete-orphan')
-    diffusion_subs        = relationship('DiffusionSubscription', cascade='all,delete,delete-orphan')
-    self_assignable_roles = relationship('SelfAssignableRole'   , cascade='all,delete,delete-orphan')
+    games                 = relationship('Game'              , cascade='all,delete,delete-orphan')
+    self_assignable_roles = relationship('SelfAssignableRole', cascade='all,delete,delete-orphan')
