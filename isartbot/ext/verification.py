825645fea4499a3a1177ddcbfc2b77b7cbfbee75
Basile Combet <21277516+Renondedju@users.noreply.github.com>
2021-11-11 19:46:14 +0100

    Finalized verification commands

--- isartbot/ext/verification.py
+++ isartbot/ext/verification.py
@@ -21,8 +21,8 @@
 # SOFTWARE.
 
 from discord.ext         import commands
+from discord.utils 	     import get
 from isartbot.checks     import is_admin
-from isartbot.helper     import Helper
 from isartbot.database   import Server
 from isartbot.converters import BetterRoleConverter
 
@@ -35,26 +35,37 @@ class VerificationExt(commands.Cog):
 	@commands.check(is_admin)
 	async def verification(self, ctx):
 		""" Verification management commands """
-		await ctx.send_help(ctx.command)
+		
+		# Prints out the current verified role
+		if ctx.invoked_subcommand is None:
+			server = self.bot.database.session.query(Server).filter(Server.discord_id == ctx.guild.id).first()
+			if server.verified_role_id == 0:
+				await ctx.send("No verified role set")
+			else:
+				verified_role = get(ctx.guild.roles, id=server.verified_role_id)
+				await ctx.send(f"Verified role is {verified_role.mention}")
 
 	@verification.command(name="set")
 	@commands.check(is_admin)
 	async def verification_set(self, ctx, role: BetterRoleConverter):
 		""" Sets the verified role for this guild in the database """
 		
-		server = self.bot.database.session.query(Server).filter(Server.id == ctx.guild.id).first()
+		server = self.bot.database.session.query(Server).filter(Server.discord_id == ctx.guild.id).first()
 		server.verified_role_id = role.id
 		self.bot.database.session.commit()
 
-		await ctx.send(f"Verified role set to {role.name}")
+		await ctx.send(f"Verified role set to {role.mention}")
 
 	@verification.command(name="disable")
 	@commands.check(is_admin)
 	async def verification_disable(self, ctx):
 		""" Disables the verification system for this guild """
 		
-		server = self.bot.database.session.query(Server).filter(Server.id == ctx.guild.id).first()
+		server = self.bot.database.session.query(Server).filter(Server.discord_id == ctx.guild.id).first()
 		server.verified_role_id = 0
 		self.bot.database.session.commit()
 
 		await ctx.send("Verification system disabled")
+
+def setup(bot):
+    bot.add_cog(VerificationExt(bot))
